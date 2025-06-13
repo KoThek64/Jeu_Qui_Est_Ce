bd0a4fa6bae11a83205195216c9ace8659b59447
E244845L <axel.doucet@etu.univ-nantes.fr>
2025-06-13 16:25:05 +0200

    modification des classes Reponse et Question

--- src/main/kotlin/Modele/Question.kt
+++ src/main/kotlin/Modele/Question.kt
@@ -1,7 +1,11 @@
 package Modele
 
-class Question(texte : String){
-    private var texte : String = texte
+class Question(texte : String = ""){
+    private var texte : String
+
+    init {
+        this.texte = texte
+    }
 
     fun getTexte(){
         println(this.texte)
