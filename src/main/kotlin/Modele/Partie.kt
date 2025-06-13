ba7161932fc270d6fd862b4f18ff7091b166dd56
loucaslbp <loucas.lebeaupin@etu.univ-nantes.fr>
2025-06-13 10:42:07 +0200

    Modif Partie.kt

--- src/main/kotlin/Modele/Partie.kt
+++ src/main/kotlin/Modele/Partie.kt
@@ -5,16 +5,16 @@ import info.but1.sae2025.data.EtatPartie
 class Partie(idPartie: Int){
     private var id : String = idPartie.toString()
     private lateinit var etat : EtatPartie
-}
 
-fun demarrerPartie(){
-    TODO()
-}
+    fun demarrerPartie(){
+        TODO()
+    }
 
-fun terminerPartie(){
-    TODO()
-}
+    fun terminerPartie(){
+        TODO()
+    }
 
-fun changerJoueurActif(){
-    TODO()
+    fun changerJoueurActif(){
+        TODO()
+    }
 }
