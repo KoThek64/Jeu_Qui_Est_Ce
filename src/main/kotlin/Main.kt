521ff925644845d918d719cb30f2024b3d8339b6
Diego <diego.goncalves-amorim@etu.univ-nantes.fr>
2025-06-16 15:25:59 +0200

    ajout et développement des controleurs rules et rulesbackbutton associé à la vue CreateJoinGame

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,17 @@
+import controleur.ControleurRules
+import controleur.ControleurRulesBackButton
 import vue.VueCreateJoinGame
 import info.but1.sae2025.QuiEstCeClient
 import javafx.application.Application
 import javafx.scene.Scene
 import javafx.stage.Stage
-import vue.VueCreatePlayer
 
 class MainQuiEstCe: Application() {
     override fun start(stage: Stage) {
         val vue = VueCreateJoinGame()
-        // vue.fixeControleurBouton()
+
+        vue.fixeControleurBouton(vue.rulesButton, ControleurRules(vue))
+        vue.fixeControleurBouton(vue.backButton, ControleurRulesBackButton(vue))
 
         val scene = Scene(vue, 1920.0, 1080.0)
         stage.title = "Qui-est-ce ?"
--- src/main/kotlin/controleur/ControleurRulesBackButton.kt
+++ src/main/kotlin/controleur/ControleurRulesBackButton.kt
@@ -4,10 +4,8 @@ import javafx.event.ActionEvent
 import javafx.event.EventHandler
 import vue.VueCreateJoinGame
 
-class ControleurRules(private val vue: VueCreateJoinGame): EventHandler<ActionEvent> {
-
+class ControleurRulesBackButton(private val vue: VueCreateJoinGame): EventHandler<ActionEvent> {
     override fun handle(event: ActionEvent) {
-        vue.rulesVue()
+        vue.baseVue()
     }
-
 }
