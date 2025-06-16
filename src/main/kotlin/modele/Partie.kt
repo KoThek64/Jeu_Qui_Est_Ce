816f2fbee6412b3d02b984a9b168897e5247f7d1
loucaslbp <loucas.lebeaupin@etu.univ-nantes.fr>
2025-06-16 15:37:55 +0200

    Partie modele

--- src/main/kotlin/modele/Partie.kt
+++ src/main/kotlin/modele/Partie.kt
@@ -2,20 +2,21 @@ package modele
 
 import info.but1.sae2025.QuiEstCeClient
 import info.but1.sae2025.data.EtatPartie
+import info.but1.sae2025.data.Joueur
 import info.but1.sae2025.data.Personnage
 
-<<<<<<< HEAD
-class Partie(private val client: QuiEstCeClient, val id: Int) {
+class Partie(
+    private val client: QuiEstCeClient,
+    private val joueurId: Int,
+    private val joueurCle: String,
+    val id: Int,
+) {
 
-    // L'état de la partie, qui sera rempli via la requête au client.
+    var selfGrille : List<List<Personnage>>? = null
+    var otherGrille : List<List<Personnage>>? = null
+
     var etat: EtatPartie? = null
 
-    val grilles: List<List<List<Personnage>>> = listOf<List<List<Personnage>>>()
-
-    /**
-     * Met à jour l'état de la partie en appelant le client.
-     * Utilise `requeteEtatPartie(idPartie)` qui retourne un objet `EtatPartie`.
-     */
 
     fun rafraichirEtat() {
         this.etat = client.requeteEtatPartie(this.id) //
@@ -23,35 +24,17 @@ class Partie(private val client: QuiEstCeClient, val id: Int) {
         chargerLesGrilles()
     }
 
-    /**
-     * Pour chaque joueur présent dans la partie, charge sa grille de personnages.
-     * Note: On suppose que `etat.joueurs` est disponible, comme dans votre code initial.
-     */
     private fun chargerLesGrilles() {
-        grilles[0] = client.requeteGrilleJoueur(this.id, this.etat.idJoueur1)
-        grilles[1] = client.requeteGrilleJoueur(this.id, this.etat.idJoueur2)
-=======
-class Partie(idPartie: Int) {
+        if (etat == null) {
+            throw Error("L'état ne doit pas être null")
+        }
 
-    private var id: Int = idPartie
-    private lateinit var etat: EtatPartie
+        val nonNullEtat = etat!!
 
-    init {
+        selfGrille = client.requeteGrilleJoueur(id, nonNullEtat.idJoueur1)
+        otherGrille = client.requeteGrilleJoueur(this.id, nonNullEtat.idJoueur2)
     }
 
-    fun demarrerPartie() {
-        TODO()
-    }
-
-    fun getEtat(): EtatPartie = etat
-
-
-    fun terminerPartie(){
-        TODO()
->>>>>>> dcedb1f53411b82cfa831e92744a068613066abe
-    }
-
-    // ... autres fonctions de jeu à implémenter, comme :
     fun poserQuestion(idJoueur: Int, cleJoueur: String, question: String) {
         client.requetePoserQuestion(this.id, idJoueur, cleJoueur, question)
     }
