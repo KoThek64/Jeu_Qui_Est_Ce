f8024e77417ebd57ca6cc9c98af64f9a42dcdce3
Mattys Lachaise <mattyslac.insta@gmail.com>
2025-06-18 12:36:01 +0200

    Ajout des tests de la requête requeteJoueurTest.kt

---
+++ src/test/kotlin/requeteJoueurTests.kt
@@ -0,0 +1,67 @@
+package test
+
+import info.but1.sae2025.QuiEstCeClient
+import info.but1.sae2025.exceptions.QuiEstCeException
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import kotlin.random.Random
+
+class requeteJoueurTests {
+    private lateinit var client: QuiEstCeClient
+
+    @BeforeEach
+    fun setup() {
+        client = QuiEstCeClient("localhost", 8080)
+    }
+
+    //-----------INITIALISATION POUR LES TESTS DE LA REQUETE DE RECUPERATION DU JOUEUR-----------//
+    fun id() : Int{
+        val listeId = client.requeteJoueurs()
+        val random = listeId.random()
+        return random
+    }
+
+    //-----------TESTS DE LA REQUETE DE RECUPERATION DU JOUEUR-----------//
+    @Test
+    fun requeteJoueurValide() {
+        val joueur = client.requeteJoueur(id())
+        assertNotNull(client.requeteJoueurs())
+        assertTrue(joueur.nom.isNotEmpty())
+        assertTrue(joueur.prenom.isNotEmpty())
+    }
+
+    @Test
+    fun requeteJoueurIdInvalide0() {
+        assertThrows<QuiEstCeException> {
+            client.requeteJoueur(0)
+        }
+    }
+
+    @Test
+    fun requeteJoueurIdValideTropPetit() {
+        val n = Random.nextInt(1,999999999)
+        assertThrows<QuiEstCeException> {
+            client.requeteJoueur(n)
+        }
+    }
+
+    @Test
+    fun requeteJoueurIdInvalideTropGrand() {
+        val n = Random.nextInt(10000000000.toInt(), Int.MAX_VALUE)
+        assertThrows<QuiEstCeException> {
+            client.requeteJoueur(n)
+        }
+    }
+
+    @Test
+    fun requeteJoueurIdInvalideNegatif() {
+        val n = -Random.nextInt(1, Int.MAX_VALUE)
+        assertThrows<IllegalArgumentException> {
+            client.requeteJoueur(n)
+        }
+    }
+
+}
