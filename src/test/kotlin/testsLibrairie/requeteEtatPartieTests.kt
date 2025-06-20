9fc610e935dc46d45e98b91d1a839798c5fd44bf
Mattys Lachaise <mattyslac.insta@gmail.com>
2025-06-20 10:25:51 +0200

    Fin des tests de requeteEtatPartieTests.kt

--- src/test/kotlin/testsLibrairie/requeteEtatPartieTests.kt
+++ src/test/kotlin/testsLibrairie/requeteEtatPartieTests.kt
@@ -1,8 +1,12 @@
 package test.testsLibrairie
 
 import info.but1.sae2025.QuiEstCeClient
+import info.but1.sae2025.exceptions.QuiEstCeException
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import kotlin.random.Random
 
 class requeteEtatPartieTests {
     private lateinit var client: QuiEstCeClient
@@ -16,7 +20,40 @@ class requeteEtatPartieTests {
     //-----------TESTS DE LA REQUETE DE RECUPERATION DE L'ETAT DE LA PARTIE-----------//
     @Test
     fun requeteEtatPartieValide() {
-        val etat = client.requeteEtatPartie(initTests.idJoueurs())
-        //TODO("à finir...")
+        val joueur = client.requeteCreationJoueur(initTests.nom(), initTests.prenom())
+        val idPartie = client.requeteCreationPartie(joueur.id, joueur.cle)
+        val etat = client.requeteEtatPartie(idPartie)
+        assertNotNull(etat)
+    }
+
+    @Test
+    fun requeteEtatPartieIdInvalideIdJoueur() {
+        assertThrows<QuiEstCeException> {
+            client.requeteEtatPartie(initTests.idJoueurs())
+        }
+    }
+
+    @Test
+    fun requeteEtatPartieInvalideIdPartieTropGrand() {
+        val n = Random.nextInt(10000000000.toInt(), Int.MAX_VALUE)
+        assertThrows<QuiEstCeException> {
+            client.requeteEtatPartie(n)
+        }
+    }
+
+    @Test
+    fun requeteEtatPartieInvalideIdPartieTropPetit() {
+        val n = Random.nextInt(1,999999999)
+        assertThrows<QuiEstCeException> {
+            client.requeteEtatPartie(n)
+        }
+    }
+
+    @Test
+    fun requeteEtatPartieInvalideIdPartieNegatif() {
+        val n = -Random.nextInt(1, Int.MAX_VALUE)
+        assertThrows<IllegalArgumentException> {
+            client.requeteEtatPartie(n)
+        }
     }
 }
